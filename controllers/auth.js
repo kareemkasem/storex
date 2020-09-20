98b8844c12a33039a60b9248aa9ad3dd0ea278ed
kareem kasem <kareemkasem@outlook.com>
2020-09-20 03:18:09 +0200

    email check

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
+const { validationResult } = require("express-validator");
 
 const User = require("../models/user");
 //................................................................
@@ -61,6 +62,15 @@ exports.postLogin = (req, res, next) => {
 
 exports.postSignup = (req, res, next) => {
   const { email, password, confirmPassword } = req.body;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status("422").render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
 
   const createUser = (email, password) => {
     return bcrypt.hash(password, 12).then((hashedPass) => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
-
 const authController = require("../controllers/auth");
+const { check } = require("express-validator");
+//...................................................................
 
 const router = express.Router();
 
@@ -10,7 +11,20 @@ router.get("/signup", authController.getSignup);
 
 router.post("/login", authController.postLogin);
 
-router.post("/signup", authController.postSignup);
+router.post(
+  "/signup",
+  check("email")
+    .isEmail()
+    .withMessage("Invalid Email")
+    .custom((input) => {
+      if (input === "kareemkasem@outlook.com") {
+        throw new Error("Admin email address is forbidden");
+      } else {
+        return true;
+      }
+    }),
+  authController.postSignup
+);
 
 router.post("/logout", authController.postLogout);
 
