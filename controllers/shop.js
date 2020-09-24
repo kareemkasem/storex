5cb82dd71a45a9b6eeb18de9aaed902aa50ee00a
kareem kasem <kareemkasem@outlook.com>
2020-09-24 06:48:46 +0200

    getting pdfs for invoices works NOT STATICALLY SERVED

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,10 @@
+// imports ...............................................................
 const Product = require("../models/product");
 const Order = require("../models/order");
+
+const fs = require("fs");
+const path = require("path");
+// .......................................................................
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -143,4 +148,17 @@ exports.getOrders = (req, res, next) => {
       error.httpStatusCode = 500;
       return next(error);
     });
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  const invoiceName = `invoice-${orderId}.pdf`;
+  const invoicePath = path.join("invoices", invoiceName);
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.send(data);
+  });
 };
